b77fcda074e5d4b48df4d0820f36a081bcba0b62
iolmstead23 <72961956+iolmstead23@users.noreply.github.com>
2023-10-11 20:35:54 -0500

    Removed firebase files and added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/ui/navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Navbar />
         {children}
       </body>
+      <Analytics />
     </html>
   )
 }
